6d9458ab038868f425cd5776f7a45a97c777c0bc
leonf08 <leonf008@gmail.com>
2024-01-18 17:10:36 +0100

    removed profiling artifacts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,24 +3,9 @@ package main
 import (
 	"github.com/leonf08/metrics-yp.git/internal/app/serverapp"
 	"github.com/leonf08/metrics-yp.git/internal/config/serverconf"
-	"os"
-	"runtime"
-	"runtime/pprof"
 )
 
 func main() {
-	fmem, err := os.Create(`profiles/result.pprof`)
-	if err != nil {
-		panic(err)
-	}
-	defer fmem.Close()
-
-	runtime.GC() // получаем статистику по использованию памяти
-
 	config := serverconf.MustLoadConfig()
 	serverapp.Run(config)
-
-	if err := pprof.WriteHeapProfile(fmem); err != nil {
-		panic(err)
-	}
 }
