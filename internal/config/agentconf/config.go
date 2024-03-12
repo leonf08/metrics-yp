38e5e2381c2d52708d8f010ae41d825513ea342f
leonf08 <leonf008@gmail.com>
2024-03-12 13:13:43 +0100

    implemented increment 22

--- internal/config/agentconf/config.go
+++ internal/config/agentconf/config.go
@@ -1,23 +1,52 @@
 package agentconf
 
 import (
-	"flag"
 	"fmt"
-	"strconv"
-	"strings"
+	"os"
 
 	"github.com/caarlos0/env/v6"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 const (
-	defaultAddress   = "localhost:8080"
-	defaultReportInt = 10
-	defaultPollInt   = 2
-	defaultKey       = ""
-	defaultRateLimit = 10
-	defaultMode      = "json"
-	defaultCryptoKey = ""
+	defaultAddress        = "localhost:8080"
+	defaultReportInt uint = 10
+	defaultPollInt   uint = 2
+	defaultRateLimit uint = 10
+	defaultMode           = "json"
 )
+
+const (
+	flagAddressName        = "address"
+	flagReportIntervalName = "report_interval"
+	flagPollIntervalName   = "poll_interval"
+	flagSignKeyName        = "auth_key"
+	flagCryptoKeyName      = "crypto_key"
+	flagConfigName         = "config"
+	flagModeName           = "mode"
+	flagRateLimitName      = "rate_limit"
+)
+
+type modeEnum string
+
+func (m *modeEnum) Set(s string) error {
+	switch s {
+	case "json", "batch", "query":
+		*m = modeEnum(s)
+		return nil
+	default:
+		return fmt.Errorf("invalid mode value: %s", s)
+	}
+}
+
+func (m *modeEnum) String() string {
+	return string(*m)
+}
+
+func (m *modeEnum) Type() string {
+	return "modeEnum"
+}
 
 // Config is a configuration for the agent
 type Config struct {
@@ -34,10 +63,10 @@ type Config struct {
 	CryptoKey string `env:"CRYPTO_KEY"`
 
 	// ReportInt is the interval for sending metrics to the server
-	ReportInt int `env:"REPORT_INTERVAL"`
+	ReportInt uint `env:"REPORT_INTERVAL"`
 
 	// PollInt is the interval for collecting metrics
-	PollInt int `env:"POLL_INTERVAL"`
+	PollInt uint `env:"POLL_INTERVAL"`
 
 	// RateLim limits the number of requests per second
 	RateLim int `env:"RATE_LIMIT"`
@@ -46,62 +75,49 @@ type Config struct {
 // MustLoadConfig loads configuration from environment variables
 // and command-line flags. If there is an error, it panics.
 func MustLoadConfig() Config {
-	address := flag.String("a", defaultAddress, "Host address of the server")
-	key := flag.String("k", defaultKey, "Authentication key")
-	rate := flag.Int("l", defaultRateLimit, "Rate limit for http requests")
-	cryptoKey := flag.String("crypto-key", defaultCryptoKey, "Path to a file with public key")
+	var mode modeEnum = defaultMode
+	pflag.VarP(&mode, flagModeName, "m", "Mode of operation, possible values: json (default), batch, query")
 
-	reportInt := defaultReportInt
-	flag.Func("r", "Report interval to server", func(s string) (err error) {
-		v, err := strconv.ParseInt(s, 10, 32)
-		if err != nil {
-			return err
+	pflag.StringP(flagAddressName, "a", defaultAddress, "Host address of the server")
+	pflag.StringP(flagSignKeyName, "k", "", "Authentication key")
+	pflag.UintP(flagRateLimitName, "l", defaultRateLimit, "Rate limit for http requests")
+	pflag.StringP(flagCryptoKeyName, "y", "", "Path to a file with public key")
+	pflag.UintP(flagReportIntervalName, "r", defaultReportInt, "Report interval to server")
+	pflag.UintP(flagPollIntervalName, "p", defaultPollInt, "Poll interval for metrics")
+	pflag.StringP(flagConfigName, "c", "", "Path to the configuration file")
+
+	pflag.Parse()
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
+		panic(err)
+	}
+
+	configFile, ok := os.LookupEnv("CONFIG")
+	if !ok {
+		configFile = viper.GetString(flagConfigName)
+	}
+
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
 		}
+	}
 
-		reportInt = int(v)
-
-		if reportInt <= 0 {
-			return fmt.Errorf("invalid flag value: should be greater than 0, got: %d", reportInt)
-		}
-
-		return nil
-	})
-	pollInt := defaultPollInt
-	flag.Func("p", "Poll interval for metrics", func(s string) (err error) {
-		v, err := strconv.ParseInt(s, 10, 32)
-		if err != nil {
-			return err
-		}
-
-		pollInt = int(v)
-
-		if pollInt <= 0 {
-			return fmt.Errorf("invalid flag value: should be greater than 0, got: %d", pollInt)
-		}
-
-		return nil
-	})
-	mode := defaultMode
-	flag.Func("m", "Mode of operation, possible values: json (default), batch, query", func(s string) error {
-		s = strings.ToLower(s)
-		if s != "json" && s != "batch" && s != "query" {
-			return fmt.Errorf("invalid flag value, got: %s", s)
-		}
-
-		mode = s
-		return nil
-	})
-
-	flag.Parse()
+	address := viper.GetString(flagAddressName)
+	key := viper.GetString(flagSignKeyName)
+	cryptoKey := viper.GetString(flagCryptoKeyName)
+	reportInt := viper.GetUint(flagReportIntervalName)
+	pollInt := viper.GetUint(flagPollIntervalName)
+	rate := viper.GetUint(flagRateLimitName)
 
 	cfg := Config{
-		Addr:      *address,
+		Addr:      address,
+		Mode:      string(mode),
+		SignKey:   key,
+		CryptoKey: cryptoKey,
 		ReportInt: reportInt,
 		PollInt:   pollInt,
-		SignKey:   *key,
-		CryptoKey: *cryptoKey,
-		RateLim:   *rate,
-		Mode:      mode,
+		RateLim:   int(rate),
 	}
 	if err := env.Parse(&cfg); err != nil {
 		panic(err)
